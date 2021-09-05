9d8d998466344940e6c9f6eccd16ee83b5ab247e
zzhanna <foto1drug@gmail.com>
2021-09-05 22:22:09 +0200

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        return Math.round(((this.max - this.min) / 2) + this.min);
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
